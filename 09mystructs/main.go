1410a4c00a09522437d5d546d4504bc8233d3793
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-09-17 18:33:35 +0530

    new things in structs

--- 09mystructs/main.go
+++ 09mystructs/main.go
@@ -24,8 +24,54 @@ func main(){
 }
 
 type User struct{
-	Name string
+	Name string 
 	Email string
 	status bool
 	Age int
 }
+
+
+type Product struct{
+	Name string
+	Price int
+	Company string
+}
+
+// func fuc(copyOfP Product){
+// 	copyOfP.Name = "Mackbook pro" // it will not change the actual product struct it will be pass by reference
+// }
+
+// func newProduct(name string, price int, company string) Product{
+// 	p:= Product{
+// 		Name: name,
+// 		Price: price,
+// 		Company: company,
+// 	}
+// 	return p // this is returing a copy of the product struct
+// }
+
+func newProduct(name string, price int, company string) *Product{
+	p:= Product{
+		Name: name,
+		Price: price,
+		Company: company,
+	}
+	return &p // this is returing the actual product struct
+}
+
+func fun_pass_by_ref(p* Product){
+	p.Name = "Mackbook pro"
+}
+// some example of copy usecases
+
+
+// member functions use pass by reference
+// member function of a struct
+
+func (p *Product) display(){
+   fmt.Println("Product details are: ", p.Name, p.Price, p.Company)
+}
+
+// call this function
+// new_p := newProduct("Iphone", 10000, "Apple")
+// new_p.display()
